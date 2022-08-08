0509ac731dda1b5ec38b3ba2c6c94c4eb6c95cd8
 Eвгений Костерин <qsterin@gmail.com>
2022-08-08 11:54:40 +0300

    initial commit

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -8,7 +8,7 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 10;
 
         long actual = service.findMax(incomesInBillions);
 
