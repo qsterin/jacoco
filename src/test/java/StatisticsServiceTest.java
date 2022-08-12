36bafb3dc3957b3fcfbaa60cc5e1d9532e698363
 Eвгений Костерин <qsterin@gmail.com>
2022-08-12 08:00:48 +0300

    Настройка обрушения сборки Jacoco

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -15,5 +15,27 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
 
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findZero() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-10,0,-3,-1};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
